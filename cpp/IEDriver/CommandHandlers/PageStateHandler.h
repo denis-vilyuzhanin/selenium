de046d7c529bba9df69139c5b67758219145cc2a
denis-vilyuzhanin <dvilyuzhanin@gmail.com>
2013-01-07 02:02:47 -0800

    Fetch DOM tree with attributes

--- cpp/IEDriver/CommandHandlers/PageStateHandler.h
+++ cpp/IEDriver/CommandHandlers/PageStateHandler.h
@@ -56,20 +56,19 @@ class PageStateHandler : public IECommandHandler {
 	Json::Value response_value;
 	response_value["url"] = url_string;
     //std::string page_source = browser_wrapper->GetPageSource();
+	Json::Value* dom = 0;
 	try {
 		response_value["bodyHtml"] = fetchBodyOuterHtml(doc);
-		
+		response_value["rootNodeName"] =  fetchRootNodeName(doc);
 		fetchRootNodeName(doc);
-		Json::Value dom;
-		dom["nodeName"] = fetchRootNodeName(doc);
-
-
-		response_value["dom"] = dom;
+		dom = fetchDOM(doc);
+		response_value["dom"] = *(dom);
 		
 	} catch (const char* m) {
 		response->SetErrorResponse(-1, m);
 		return;
-	}
+	} 
+		
 
     response->SetSuccessResponse(response_value);
   }
@@ -85,6 +84,8 @@ class PageStateHandler : public IECommandHandler {
 	std::string bodyHTMLString = CW2A(outerHTML, CP_UTF8);
 	return bodyHTMLString;
   }
+
+  
 
   std::string fetchRootNodeName(CComPtr<IHTMLDocument2>& doc) {
 	  CComPtr<IHTMLDocument3> doc3;
@@ -105,12 +106,123 @@ class PageStateHandler : public IECommandHandler {
 
 	  std::string rootNodeNameString = CW2A(rootNodeName, CP_UTF8);
 	  return rootNodeNameString;
-	  /*CComPtr<IHTMLHtmlElement> htmlElement;
+	  CComPtr<IHTMLHtmlElement> htmlElement;
 	  hr = documentElement->QueryInterface(&htmlElement);
-	  assertSuccess(hr, "Unable to get html element");*/
+	  assertSuccess(hr, "Unable to get html element");
 
 
   }
+
+  void fetchRootElement(CComPtr<IHTMLElement>& rootElement, CComPtr<IHTMLDocument2>& doc) {
+	  CComPtr<IHTMLDocument3> doc3;
+	  HRESULT hr = doc->QueryInterface(&doc3);
+	  assertSuccess(hr, "Unable to get document3");
+	  
+	  hr = doc3->get_documentElement(&rootElement);
+	  assertSuccess(hr, "Unable to get document element");	
+  }
+
+	Json::Value* fetchDOM(CComPtr<IHTMLDocument2>& doc) {
+		CComPtr<IHTMLElement> rootElement;
+		fetchRootElement(rootElement, doc);
+
+		return fetchElement(rootElement);
+	}
+
+	Json::Value* fetchElement(CComPtr<IHTMLElement>& element) {
+		CComPtr<IHTMLDOMNode> node;
+		element->QueryInterface(&node);
+		return fetchElement(node);
+	}
+
+	Json::Value* fetchElement(CComPtr<IHTMLDOMNode>& domNode) {
+		HRESULT hr;
+		Json::Value* jsonElement = new Json::Value();
+		
+		CComBSTR nodeName;
+		domNode->get_nodeName(&nodeName);
+		std::string nodeNameString = CW2A(nodeName, CP_UTF8);
+		(*jsonElement)["nodeName"] = nodeNameString;
+
+		long nodeType;
+		domNode->get_nodeType(&nodeType);
+		(*jsonElement)["nodeType"] = nodeType;
+
+		CComVariant nodeValue;
+		domNode->get_nodeValue(&nodeValue);
+		if (nodeValue.vt == VT_BSTR) {
+			std::string nodeValueString = CW2A(nodeValue.bstrVal, CP_UTF8);
+			(*jsonElement)["nodeValue"] = nodeValueString;
+		}
+
+		/*Json::Value* jsonAttributes = fetchDOMAttribute(domNode);
+		if (jsonAttributes != NULL) {
+			(*jsonElement)["attributes"] = *jsonAttributes;
+		}*/
+
+		Json::Value* jsonChildren = new Json::Value(Json::arrayValue);
+		CComPtr<IHTMLDOMNode> childDomNode;
+		hr = domNode->get_firstChild(&childDomNode);
+		for(;childDomNode != 0;) {
+			Json::Value* jsonChildElement = fetchElement(childDomNode);
+			(*jsonChildren).append(*jsonChildElement);
+			
+			CComPtr<IHTMLDOMNode> temp;
+			hr = childDomNode->get_nextSibling(&temp);
+			childDomNode = temp;
+		}
+		(*jsonElement)["children"] = (*jsonChildren);
+		return jsonElement;
+	}
+
+
+	Json::Value* fetchDOMAttribute(CComPtr<IHTMLDOMNode>& domNode) {
+		Json::Value* jsonAttributes = new Json::Value();
+		CComPtr<IDispatch> domAttributesDispatch;
+		HRESULT hr = domNode->get_attributes(&domAttributesDispatch);
+
+		if (domAttributesDispatch == NULL) {
+			return NULL;
+		}
+
+		CComPtr<IHTMLAttributeCollection> domAttributes;
+		hr = domAttributesDispatch->QueryInterface(&domAttributes);
+
+		long domAttributesCount;
+		hr = domAttributes->get_length(&domAttributesCount);
+
+		VARIANT attributeIndex;
+		attributeIndex.vt = VT_I4;
+		for(int i = 0; i < domAttributesCount; i++) {
+			attributeIndex.lVal = i;
+			CComPtr<IDispatch> domAttributeDispatch;
+			hr = domAttributes->item(&attributeIndex, &domAttributeDispatch);
+
+			CComPtr<IHTMLDOMAttribute> domAttribute;
+			hr = domAttributeDispatch->QueryInterface(&domAttribute);
+
+			VARIANT_BOOL isSpecified;
+			hr = domAttribute->get_specified(&isSpecified);
+			if(!isSpecified) {
+				continue;
+			}
+			CComBSTR domAttributeName;
+			domAttribute->get_nodeName(&domAttributeName);
+			const std::string domAttributeNameString = CW2A(domAttributeName, CP_UTF8);
+			
+			CComVariant domAttributeValue;
+			domAttribute->get_nodeValue(&domAttributeValue);
+			
+			if (domAttributeValue.vt == VT_BSTR) {
+				std::string domAttributeValueString = CW2A(domAttributeValue.bstrVal, CP_UTF8);
+				(*jsonAttributes)[domAttributeNameString]= domAttributeValueString;
+			} else {
+				(*jsonAttributes)[domAttributeNameString] = Json::nullValue;
+			}
+		}
+		return jsonAttributes;
+	}
+
 
   void assertSuccess(HRESULT hr, const char* message) {
 	if (FAILED(hr)) {
